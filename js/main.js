e1061772ebf7e1a3cc81590a4918059c9ce9fc8d
George Di Iorio <62784063+georgediiorio@users.noreply.github.com>
2021-12-04 10:21:32 -0800

    Added all files.

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// LightGallery script
+lightGallery(document.querySelector('.gallery'), {
+    licenseKey: '69AC746A-62354883-977B764B-C1A34D5F',
+    closeOnTap: true,
+    escKey: true,
+})
+
+// Copyright footer
+let year = new Date().getFullYear();
+let copy = `&copy; George DiIorio Photography ${year}.`;
+document.getElementById('main-footer').innerHTML = copy;
+
+//Modal
+const modal = document.getElementById('contact-modal')
+const openBtn = document.querySelector('.main-btn')
+const closeBtn = document.querySelector('.close-btn')
+
+openBtn.addEventListener('click', () => {
+    modal.style.display = 'block'
+})
+
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none'
+})
+
+window.addEventListener('click', (e) => {
+    if(e.target === modal) {
+        modal.style.display = 'none'
+    }
+})
+
+//Form Validation
+//const form = document.getElementById('form')
+//const name = document.getElementById('name')
+//const email = document.getElementById('email')
+//const message = document.getElementById('message')
